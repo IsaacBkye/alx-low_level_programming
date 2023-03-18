27a1bb3df759759603ad91b1dab7030116a5d2cd
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-17 20:42:11 -0700

    Task 4-6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@ int main(void)
 {
 	char str[50] = "Programming is like building a multilingual puzzle";
 
-	puts(str);
+	puts(str"\n");
 
 	return(0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/**
+ * Printing with prinf inbuilt fn 
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
