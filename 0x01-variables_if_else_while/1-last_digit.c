3c225b76557d67ab1003037474fc353067c0b8f2
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-18 05:58:39 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,16 +8,15 @@
 int main(void)
 {
 	int n;
-	char m[];
+	int m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = (char)n;
-	int o = m[sizeof(m) / sizeof(char)];
+	m = n % 10;
 	char p[] = "and is greater than 5";
 	char q[] = "and is 0";
 	char r[] = "and is less than 6 and not 0";
 
-	printf("%c ", "%d ", "is ", "%s\n", o, n, o > 5 ? p : o == 0 ? q : r);
+	printf("%c ", "%d ", "is ", "%s\n", m, n, m > 5 ? p : m == 0 ? q : r);
 	return (0);
 }
