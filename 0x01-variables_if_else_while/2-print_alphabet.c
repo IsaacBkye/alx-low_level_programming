2c52db3b05601038ce769535384274f7e70a4c10
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-18 06:38:04 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in  lowercase
+ * Return: Nothing
+ */
+int main(void)
+{
+	char a = "";
+
+	for (int b = 0, b < 26, b++)
+	{
+		a = (char)b;
+		putchar(a);
+	}
+}
