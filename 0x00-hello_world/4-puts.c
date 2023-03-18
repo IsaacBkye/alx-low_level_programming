9b84d5be8e72d34d690f25a151de0ddcfd16ffce
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-17 21:50:12 -0700

    Tasks 4-6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,10 @@
 #include <stdio.h>
 int main(void)
 {
+	char a = "\"";
 	char strr[] = "Programming is like building a multilingual puzzle\n";
 
-	puts("\""+strr);
+	puts(a);
+	puts(strr);
 	return (0);
 }
