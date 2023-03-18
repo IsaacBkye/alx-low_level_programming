5864e974a6db56bd78ae6035b3a5fa9e036fd85b
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-18 07:14:13 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets in both low and uppercases
+ * Return: Nothing
+ */
+int main(void)
+{
+	char a = 'a';
+	int b;
+	int c;
+	char d;
+
+	for (b = 0; b < 2 b++)
+	{
+		for (c = (int)a; c < (int)a +26 ; c++)
+		{
+			d = c;
+			putchar(d);
+		}
+		a = 'A';
+	}
+	d = '\n';
+	putchar(d);
+	return (0);
+}
