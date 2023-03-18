621635901bfb37a3ca46ff0ac0352c0c462fc0df
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-18 07:28:05 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets except e and q
+ * Return: Nothing
+ */
+int main(void)
+{
+	int a = 'a';
+	int b;
+
+	for (b = a, b < b + 26; b++)
+	{
+		if ((char)b != (q || e))
+		{
+			c = (char)b;
+			putchar(c);
+		}
+	}
+	c = '\n';
+	putchar(c);
+	return (0);
+}
