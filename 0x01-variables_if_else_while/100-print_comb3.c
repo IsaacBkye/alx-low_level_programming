a2f1965533b753056e966025f0214bd1885c7bf2
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-20 20:54:01 -0700

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print a 2-digit combination
+ * Return: Nothing
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar(a);
+			putchar(b);
+			putchar(',');
+			putchar(' ');
+		}
+	
+	}
+	putchar('\n');
+	return (0);
+}
