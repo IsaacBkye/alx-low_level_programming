9324f81d6884c3df4c0e93f68b0fc0059939ea50
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-21 21:13:21 -0700

    Task 5

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -52,3 +52,19 @@ int _isalpha(int c)
 	else
 		return (0);
 }
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}else
+	{
+		putchar('0');
+		return (0);
+	}
+}
