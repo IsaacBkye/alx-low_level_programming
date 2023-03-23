ba3025543e6f30944c1b86d3b8c7a5f02a9f2d1d
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-22 21:51:26 -0700

    Tasks revisted

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,24 +1,31 @@
 #include "main.h"
 /**
- * void times_table - print 9 xtable
+ * times_table - print 9 xtable
  *
+ * Return: void
  */
 void times_table(void)
 {
 	int a;
 	int b;
+	int c;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			_putchar('0' + (a * b));
+			c = a * b;
+			if (c > 10)
+			{
+				_putchar('0' + (c / 10));
+			}
+			_putchar('0' + (c % 10));
 			if (b != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
