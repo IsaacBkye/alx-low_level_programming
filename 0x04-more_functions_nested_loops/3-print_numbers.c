3854f86c530a0871c67964fcfa5fc15ab0efb93c
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-23 23:02:22 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print
+ * Return: Nothing
+ */
+void print_numbers(void);
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar('0' + a);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,5 +7,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+int_putchar(char c);
+void print_numbers(void);
 
 #endif
