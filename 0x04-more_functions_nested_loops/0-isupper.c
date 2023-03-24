e377d3c4f0fc04f402aec3e05ebe2de0ecab994f
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-23 21:12:06 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/**
+ * isupper - Checks for uppercase
+ *
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	int a;
+	
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		if (c == a)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
