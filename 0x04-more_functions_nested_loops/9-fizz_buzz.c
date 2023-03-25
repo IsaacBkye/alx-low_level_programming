703acbca390e79c1a3372076f2eedc8f25f83d9c
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-24 21:46:51 -0700

    Task revisited

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,6 +11,8 @@ int main(void)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
 			printf("FizzBuzz ");
+		else if (a == 100)
+			printf("Buzz");
 		else if (a % 3 == 0)
 			printf("Fizz ");
 		else if (a % 5 == 0)
