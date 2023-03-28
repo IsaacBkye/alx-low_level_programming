6240f7a31c6b183c567e4faaacae1b11503627f4
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-28 16:48:54 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - Prints in reverse
+ * @s: arg
+ */
+void print_rev(char *s)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10000; a++)
+	{
+		if (*(s + a) == '\0')
+			break;
+	}
+	for (b = a; b > -1; b--)
+	{
+		_putchar(*(s + b));
+	}
+	_putchar('\n');
+}
