cb75eaf9261f0f91656d771048d68d686b781b1c
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-28 16:22:22 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - Count string length
+ *
+ * @s: arg
+ * Return: The length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (*(s + i) == '\0')
+			break;
+	}
+	return (i);
+}
