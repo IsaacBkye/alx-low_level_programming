222c64285372e53c3c84403b4d68ea1a34ef6a5e
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-28 22:53:58 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_array - Prints in reverse
+ * @a: arg
+ * @n: arg
+ */
+void print_array(int *a, int n)
+{
+	int d;
+	int b;
+
+	for (d = 0; d < n; d++)
+	{
+		_putchar(*(s + d * 4));
+	}
+	_putchar('\n');
+}
