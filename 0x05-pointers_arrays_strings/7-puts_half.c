096152936eb74b54a4d5d1baf529be453d4c91e3
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-28 22:42:37 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - Prints in reverse
+ * @str: arg
+ */
+void puts_half(char *str))
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10000; a++)
+	{
+		if (*(str + a) == '\0')
+			break;
+	}
+	for (b = (a - 1) / 2; b <= a; b++)
+	{
+		_putchar(*(str + b));
+	}
+	_putchar('\n');
+}
