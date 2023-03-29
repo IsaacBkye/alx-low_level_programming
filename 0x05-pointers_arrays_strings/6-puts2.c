562eb243b277306ce0681249f2b0ea405aa584e3
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-28 22:38:05 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - Prints in reverse
+ * @str: arg
+ */
+void puts2(char *str))
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10000; a++)
+	{
+		if (*(str + a) == '\0')
+			break;
+	}
+	for (b = 0; b <= a; b++)
+	{
+		_putchar(*(str + b));
+		b++;
+	}
+	_putchar('\n');
+}
