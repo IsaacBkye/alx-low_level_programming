e0bb437766b37c1ed5862015e2ebf4539dc9a8ab
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-29 19:59:46 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@ void rev_string(char *s)
 {
 	int i;
 	int a = 0;	
-	char *c;
+	char *c = s;
 
 	for (i = 0; i < 1000; i++)
 	{
@@ -15,7 +15,7 @@ void rev_string(char *s)
 			break;
 		*(c + i) = *(s + i);
 	}
-	for (i = i; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 	{
 		*(s + a) = *(c + i);
 		a++;
