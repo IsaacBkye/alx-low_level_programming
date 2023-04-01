81e1bbd787ead89e3f5d7227a70cc0f89753c2a3
Isaac Boakye <isaacboakye77@outlook.com>
2023-03-31 21:03:15 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strcat - Concatenates strings
+ * @dest: arg
+ * @src: arg1
+ * Return: pointer to char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int k;
+	int j;
+	int l;
+
+	for (i = 0; i < 1000000; i++)
+	{
+		if (*(dest + i) == '\0')
+			break;
+	}
+	j = i;
+	for (k = 0; k < 1000000; k++)
+	{
+		if(*(src + k) == '\0')
+			break;
+	}
+	l = j;
+	for (i = i; i < j + k; i++)
+	{
+		*(dest + i) = *(src + (i - l));
+	}
+	return (dest);
+}
