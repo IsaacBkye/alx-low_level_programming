93bd800801082ff6139007ff3f985b0a7f3b536f
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-12 10:59:14 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strdup - returns a pointer to a new string which is a duplicate of the string
+ * _strdup - returns a pointer to a new string
  * @str: arg
  * Return: Pointer to char or NULL
  */
@@ -10,6 +10,8 @@ char *_strdup(char *str)
 	int i;
 	int j = 0;
 
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; i < 100000; i++)
 	{
 		j++;
