aad47d5b600a430d24caa5db10e10744cdf357f6
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-12 09:56:53 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
  * create_array - Creates an array of chars
- * size: arg
- * c: arg1
- * Return: Pointer to char or Null if size is 0
+ * @size: arg
+ * @c: arg1
+ * Return: Pointer to char or Null if size is
  */
 char *create_array(unsigned int size, char c)
 {
 	char *a;
 	unsigned int i;
-       
+
 	if (size != 0)
 	{
-		a = (char*)malloc(size);
+		a = (char *)malloc(size);
 		if (a == NULL)
 			return (NULL);
 		for (i = 0; i < size; i++)
