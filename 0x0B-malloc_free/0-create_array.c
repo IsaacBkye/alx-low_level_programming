b6b6ec5dcea9d0f23517c165394a875b92698210
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-12 09:50:51 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,8 @@ char *create_array(unsigned int size, char c)
 	if (size != 0)
 	{
 		a = (char*)malloc(size);
+		if (a == NULL)
+			return (NULL);
 		for (i = 0; i < size; i++)
 		{
 			*(a + i) = c;
