ba2f0c1308a1cdff5e86a4a876b0c18c25612ae7
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-12 21:18:25 -0700

    Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * free_grid - Free a 2d grid
+ * @grid: arg
+ * @height:arg1
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	
+	for (i = 0; i <= height; i++)
+	{
+		free(grid[i]);
+	}
+}
