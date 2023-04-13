45bc2b0b5888ea226eea585364784a1ee6b1afa3
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-12 18:38:19 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,75 @@
 #include "main.h"
 /**
+ * pCopy - Copy for pointer
+ * @p: arg
+ * Return: Char to pointer or Null
+ */
+char *pCopy(char *p)
+{
+	int i;
+	int j = 0;
+	char *a;
+
+	for (i = 0; i < 100000; i++)
+	{
+		j++;
+		if (p[i] == '\0')
+			break;
+	}
+	a = (char *)malloc(j * sizeof(*p));
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++)
+	{
+		a[i] = p[i];
+	}
+	return (a);
+}
+
+/**
+ * _pCopy - Copy for 2 pointers as one
+ * @p1: arg
+ * @p2: arg1
+ * Return: Char to pointer or Null
+ */
+char *_pCopy(char *p1, char *p2)
+{
+	int j = 0;
+	int k = 0;
+	int i;
+	int l;
+	char *a;
+
+	for (i = 0; i < 100000; i++)
+	{
+		j++;
+		if (p1[i] == '\0')
+			break;
+	}
+	for (i = 0; i < 100000; i++)
+	{
+		k++;
+		if (p2[i] == '\0')
+			break;
+	}
+	a = (char *)malloc(k * sizeof(*p2) + j * sizeof(*p1));
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < 100000; i++)
+	{
+		if (p1[i] == '\0')
+			break;
+		a[i] = p1[i];
+	}
+	l = i;
+	for (i = l; i < j + k; i++)
+	{
+		a[i] = *(p2 + i - l);
+	}
+	return (a);
+}
+
+/**
  * str_concat - Concatenate strings by pointers
  * @s1: arg
  * @s2: arg1
@@ -7,45 +77,21 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i;
-	int j = 0;
-	int k = 0;
-	int l;
 	char *a;
 
 	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
-
-	for (i = 0; i < 100000; i++)
+	if (s1 == NULL)
 	{
-		j++;
-		if (s1[i] == '\0')
-		{
-			break;
-		}
-	}
-	for (i = 0; i < 100000; i++)
+		a = pCopy(s2);
+		return (a);
+	} else if (s2 == NULL)
 	{
-		k++;
-		if (s2[k] == '\0')
-			break;
+		a = pCopy(s1);
+		return (a);
 	}
-	a = (char *)malloc(k * sizeof(*s2) + j * sizeof(s1));
-	if (a == NULL)
-		return (NULL);
-	for (i = 0; i < j; i++)
-	{
-		if (s1[i] == '\0')
-			a[i] = ' ';
-		else
-			a[i] = s1[i];
-	}
-	l = i;
-	for (i = l; i < j + k; i++)
-	{
-		a[i] = *(s2 + i - l);
-	}
+	a = _pCopy(s1, s2);
 	return (a);
 }
