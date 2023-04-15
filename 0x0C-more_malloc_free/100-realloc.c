b00e35e49bd9043cafaed13b0e393ab9b3749ef5
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-14 18:23:13 -0700

    Task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _realloc - Reallocates a new block of memory
+ * @ptr: arg1
+ * @old_size: arg2
+ * @new_size: arg3
+ */
+void *_realloc(char *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *a;
+	int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	else if (new_size == 0 && ptr != NULL)
+		free(ptr);
+	else if (ptr == NULL)
+	{
+		a = malloc(new_size);
+		if (a == NULL)
+			return (NULL);
+		return (a);
+	} else
+	{
+		a = malloc(old_size + new_size);
+		if (a == NULL)
+			return (NULL);
+		for (i = 0; ptr[i] != '\0'; i++)
+			a[i] = ptr[i];
+		a[i] = '\0';
+		free(ptr);
+		return (a);
+	}
+	return (NULL);
+}
