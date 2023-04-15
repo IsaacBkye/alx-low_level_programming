3678d17d580c1144bfb1bef772bf3f3698af9e80
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-14 18:27:55 -0700

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,6 +4,7 @@
  * @ptr: arg1
  * @old_size: arg2
  * @new_size: arg3
+ * Return: pointer
  */
 void *_realloc(char *ptr, unsigned int old_size, unsigned int new_size)
 {
@@ -13,23 +14,23 @@ void *_realloc(char *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	else if (new_size == 0 && ptr != NULL)
+	{
 		free(ptr);
+		return (NULL);
+	}
 	else if (ptr == NULL)
 	{
 		a = malloc(new_size);
 		if (a == NULL)
 			return (NULL);
 		return (a);
-	} else
-	{
-		a = malloc(old_size + new_size);
-		if (a == NULL)
-			return (NULL);
-		for (i = 0; ptr[i] != '\0'; i++)
-			a[i] = ptr[i];
-		a[i] = '\0';
-		free(ptr);
-		return (a);
 	}
-	return (NULL);
+	a = malloc(old_size + new_size);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; ptr[i] != '\0'; i++)
+		a[i] = ptr[i];
+	a[i] = '\0';
+	free(ptr);
+	return (a);
 }
