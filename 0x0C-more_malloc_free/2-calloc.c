4acbcbdeae42624e17af4a8ca1f5b0c681094ad6
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-14 17:39:40 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,12 +9,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *a = (char *)malloc(nmemb * size);
+	int i;
 
 	if (a == NULL)
 		return (NULL);
 	else if (nmemb != 0 && size != 0)
 	{
-		memset(a, 0, nmemb);
+		for (i = 0; i < (int)size; i++)
+			a[i] = 0;
 		return (a);
 	}
 	return (NULL);
