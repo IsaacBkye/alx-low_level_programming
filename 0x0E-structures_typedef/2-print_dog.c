c2fdb08c135e161f85ea6cf83899ea47620de8cd
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-17 21:32:11 -0700

    Task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog
+ * @d: arg
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL || d->owner == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("Name: %s\nAge: %1f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
+}
