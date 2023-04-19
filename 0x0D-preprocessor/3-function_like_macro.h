a2d7e26670149e2d6aa8c65481f2f1f820fcebf1
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-18 19:25:01 -0700

    Task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#include <stdlib.h>
+/**
+ * ABS - A Macro
+ * @x: arg
+ */
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+#endif /* FUNCTION_LIKE_MACRO_H */
