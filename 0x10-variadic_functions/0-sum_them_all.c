aeb1173b805a540e3447eb22782ab777cb394b95
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-20 18:50:50 -0700

    Task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - Sums all args
+ * @n: arg
+ * Return: result
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	unsigned int summed = 0;
+	va_list ls;
+	
+	if (n == 0)
+		return (0);
+	va_start(ls, n);
+	for (i = 0; i < n; i++)
+		summed += va_arg(ls, unsigned int);
+	va_end(ls);
+	return (summed);
+}
