e2c906a393b08cd97f3b8835c3c8cdcc1c50ea41
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-27 20:33:07 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,11 +8,14 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *a = (list_t *)malloc(sizeof(head));
+	int i = 0;
 
 	if (a == NULL || head == NULL)
 		return (NULL);
 	a->str = strdup(str);
-	a->len = strlen(str);
+	while (str[i] != '\0')
+		i++;
+	a->len = i;
 	a->next = *head;
 	*head = a;
 	return (*head);
