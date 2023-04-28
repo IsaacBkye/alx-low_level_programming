faa3f03b1b13380e48fce7a5ad31b70483b4b0a7
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-27 18:23:46 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * print_list - Prints number of nodes
+ * @h: lists passed
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 1;
+	list_t *a = (list_t *)malloc(sizeof(h));
+
+	if (a == NULL)
+		return (-1);
+	if (h == NULL)
+	{
+		printf("[0] (nil)\n");
+		return(0);
+	}
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+	a = h->next;
+	while (a != NULL)
+	{
+		if (a->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", a->len, a->str);
+		a = a->next;
+		i++;
+	}
+	return (i);
+}
