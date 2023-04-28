46c33bd81740cfc827caf1bc950cf3238b44ceb9
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-27 20:30:24 -0700

    Task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_node_end - Adds a nodes
+ * @head: Pointer to head
+ * @str: String to put in element
+ * Return: Address of new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *a = (list_t *)malloc(sizeof(head));
+	list_t *b = (list_t *)malloc(sizeof(head));
+	int i = 0;
+
+	if (a == NULL || head == NULL || b == NULL)
+		return (NULL);
+	a->str = strdup(str);
+	while(str[i] != '\0')
+		i++;
+	a->len = i;
+	if (*head == NULL)
+	{
+		*head = a;
+		a->next = NULL;
+		return (*head);
+	}
+	b = *head;
+	while (b->next != NULL)
+		b = b->next;
+	b->next = a;
+	a->next = NULL;
+	return (*head);
+}
