2da9bf18cae66081a26bea4ff3166ab871563c71
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-27 19:20:56 -0700

    Task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_node - Adds a nodes
+ * @head: Pointer to head
+ * @str: String to put in element
+ * Return: Address of new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *a = (list_t *)malloc(sizeof(head));
+
+	if (a == NULL)
+		return (0);
+	if (head == NULL)
+	{
+		return (0);
+	}
+	a->str = strdup(str);
+	a->len = strlen(str);
+	a->next = *head;
+	*head = a;
+	printf("[%d] %s\n", a->len, a->str);
+	return (*head);
+}
