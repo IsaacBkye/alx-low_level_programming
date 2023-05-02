208c094c81962cfd50f0002fbb1f02db88f7fe06
Isaac Boakye <isaacboakye77@outlook.com>
2023-05-01 20:09:55 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - Prints elements of a list
+ * @h: arg
+ * Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t elements = 0;
+	
+	while (h)
+	{
+		elements += 1;
+	       	printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (elements);
+}
