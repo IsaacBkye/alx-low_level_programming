cb6bd8f96924d33bbc89a1778bc9d4ab3606ed23
Isaac Boakye <isaacboakye77@outlook.com>
2023-05-02 18:53:02 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * free_listint2 - Frees a list
+ * @head: arg
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *a = NULL;
+
+	if (*head == NULL)
+	{
+		return;
+	} else if ((*head)->next == NULL)
+	{
+		free(*head);
+		head = NULL;
+	} else
+	{
+		while (*head != NULL)
+		{
+			a = *head;
+			*head = a->next;
+			free(a);
+		}
+	}
+}
