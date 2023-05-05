aeb892a0f0b4f022bc2faa6cbeba240a89497ccc
Isaac Boakye <isaacboakye77@outlook.com>
2023-05-04 18:36:57 -0700

    Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * binary_to_unit - Converts to unit
+ * @b: arg
+ * Return: unit result
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int a = 0;
+
+	if (*b < '0' || *b > '1' || b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		if (*b == '1')
+		{
+			a = 1;
+			b++;
+			while (*b != '\0')
+			{
+				if (*b < '0' || *b > '1')
+					return (0);
+				a = a << 1;
+				if (*b == '1')
+					a += 1;
+				b++;
+			}
+			break;
+		}
+		b++;
+	}
+	return (a);
+}
