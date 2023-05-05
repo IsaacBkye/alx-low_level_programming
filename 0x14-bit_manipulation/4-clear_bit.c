eb84f3101e2d22e6200e045751024d1178c2ea2c
Isaac Boakye <isaacboakye77@outlook.com>
2023-05-04 21:20:42 -0700

    Task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * clear_bit - Set value of bit to 0
+ * @index: arg
+ * Return: 1 if a success or 0 otherwise
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int a;
+
+	a = 1 << index;
+	if (index <= sizeof(n) * 8 - 1)
+	{
+		*n &= ~a;
+		return (1);
+	}
+	return (-1);
+}
