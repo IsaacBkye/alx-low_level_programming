8eea72dd350bee739113b096fb8e31fe83d50549
Isaac Boakye <isaacboakye77@outlook.com>
2023-05-04 21:02:04 -0700

    Task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * expo - Calculate exponent
+ * @a: arg
+ * @b: arg1
+ * Return: Result
+ */
+unsigned int expo(unsigned int a, unsigned int b)
+{
+	unsigned int i, res = 1;
+
+	for (i = 0; i < b; i++)
+		res = res * a;
+	if (b == 0)
+		return (1);
+	return (res);
+}
+/**
+ * set_bit - Set bit at given index
+ * @n: arg
+ * @index: arg1
+ * Return: 1 if successful 0 otherwise
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+		return (-1);
+	*n = *n | expo(2, index);
+	return (1);
+}
