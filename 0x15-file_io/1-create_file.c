d758fa3b34a8524c263ff81913d444b0578e600f
Isaac Boakye <isaacboakye77@outlook.com>
2023-05-08 22:53:48 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_filee - Creates a file
+ * @filename: arg
+ * @text_content: arg1
+ * Return: 0 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int filepointer;
+	int len = 0;
+	int filewrite;
+
+	filepointer = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 400);
+	if (filepointer == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	while (text_content[len] != '\0')
+		len++;
+	filewrite = write(filepointer, text_content, len);
+	if (filewrite == -1)
+		return (-1);
+	close(filepointer);
+	return (1);
+}
