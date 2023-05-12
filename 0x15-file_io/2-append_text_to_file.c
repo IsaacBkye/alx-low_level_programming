efdd45d434e480c6631c1c9f1c1f0f02d9568d30
Isaac Boakye <isaacboakye77@outlook.com>
2023-05-11 21:58:21 -0700

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	fileopen = open(filename, O_RDWR | O_APPEND);
 	if (fileopen == -1)
 		return (-1);
-	if ((getFlag = fcntl(fileopen, F_GETFL)) == O_RDONLY)
+	getFlag = fcntl(fileopen, F_GETFL) & O_ACCMODE;		
+	if (getFlag == O_RDONLY)
 		return (-1);
 	if (text_content != NULL)
 	{
