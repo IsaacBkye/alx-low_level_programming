241f492f99c796550224f3a71f5000209088c3e2
Isaac Boakye <isaacboakye77@outlook.com>
2023-05-11 17:53:06 -0700

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,11 +9,11 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int filewrite;
 	int fileopen;
-	int len;
+	int len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fileopen = open(filename, O_APPEND | O_RDWR);
+	fileopen = open(filename, O_RDWR | O_APPEND);
 	if (fileopen == -1)
 		return (-1);
 	if (text_content == NULL)
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	filewrite = write(fileopen, text_content, len);
 	if (filewrite == -1)
 		return (-1);
-	filewrite = write(fileopen, "\n", 1);
+	filewrite = write(fileopen, "\n\0", 2);
 	if (filewrite == -1)
 		return (-1);
 	close(fileopen);
