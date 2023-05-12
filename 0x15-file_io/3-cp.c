1f416d88a036af98d63a1b06cc0da9a050a9b274
Isaac Boakye <isaacboakye77@outlook.com>
2023-05-11 19:08:47 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,29 +12,29 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fileopen1 = open(argv[1], O_RDONLY);
 	fileopen2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0644);
-	do
-	{
+	do {
 		fileread = read(fileopen1, buffer, 1024);
 		if (fileread == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		filewrite = write(fileopen2, buffer, fileread);
 		if (filewrite == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n",
+				       argv[2]);
 			exit(99);
 		}
 	} while (fileread == 1024);
 	if (close(fileopen1) == -1 || close(fileopen2) == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd FD_VALUE %d\n", -1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE %d\n", -1);
 		exit(100);
 	}
 	return (0);
