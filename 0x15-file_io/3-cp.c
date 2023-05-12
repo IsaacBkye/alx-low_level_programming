df22ad1c189d210d49b932a3681d7651ac0c3f2a
Isaac Boakye <isaacboakye77@outlook.com>
2023-05-11 19:21:52 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,16 @@
 #include "main.h"
 /**
+ * errr - Prints to stderr
+ */
+void errr(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+/**
  * main - imitates cp system call
- * argc - number of passed args
- * argv - values of passed args
+ * @argc: number of passed args
+ * @argv: values of passed args
  * Return: 97, 98, 99, 100
  */
 int main(int argc, char *argv[])
@@ -11,12 +19,19 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		errr();
 	fileopen1 = open(argv[1], O_RDONLY);
+	if (fileopen1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	fileopen2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	if (fileopen2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(98);
+	}
 	do {
 		fileread = read(fileopen1, buffer, 1024);
 		if (fileread == -1)
