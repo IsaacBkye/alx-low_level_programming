652a3984932d0837abe7ac6841f9de28c55579a9
Isaac Boakye <isaacboakye77@outlook.com>
2023-06-15 19:06:25 -0700

    Task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * get_node_rec - get the nth node of dlistint_t linked list recursively.
+ * @head: pointer list.
+ * @n: nodes acumlated as param.
+ * @index: index node to return.
+ * Return: nth node.
+ */
+dlistint_t *getNode1(dlistint_t *head, unsigned int index, unsigned int n)
+{
+	if (head)
+	{
+		if (n == index)
+			return (head);
+		head = getNode1(head->next, index, n + 1);
+	}
+	return (head);
+}
+/**
+ * get_dnodeint_at_index - get the nth node oa dlistint_t linked list.
+ * @head: double pointer list.
+ * @index: index wanted node.
+ * Return: the wanted node by index.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int a = 0;
+
+	return (getNode1(head, index, a));
+}
