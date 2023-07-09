4e02209fc8ac8193060cd1139d9768eab951ff0a
Isaac Boakye <isaacboakye77@outlook.com>
2023-07-08 17:03:03 -0700

    Task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -7,13 +7,11 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long hash = 5381;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash);
 }
