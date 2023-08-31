5806a25afd043166c14008d5ec8c7c27af470438
Isaac Boakye <isaacboakye77@outlook.com>
2023-08-31 08:04:01 -0700

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,10 +3,20 @@
 Island perimeter Module
 """
 
-
 def island_perimeter(grid):
-    """
-    To calculate the perimeter of character island
-    """
+    """Python function to return the perimeter of an island"""
 
-    return 12
+    p = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                if x + 1 == len(grid) or grid[x + 1][y] == 0:
+                    p += 1
+                if y + 1 == len(grid[0]) or grid[x][y + 1] == 0:
+                    p += 1
+                if x - 1 == -1 or grid[x - 1][y] == 0:
+                    p += 1
+                if y - 1 == -1 or grid[x][y - 1] == 0:
+                    p += 1
+    return(p)
+
